808cb8e5374d452b20316c317b23b22753458380
Karime <karimehq3@gmail.com>
2026-03-12 14:44:45 -0600

    correccion

--- Practice4/DES.py
+++ Practice4/DES.py
@@ -1,6 +1,7 @@
 import base64
 
 
+
 def encode_to_base64(datos_binarios):
  
     # Dato de entrada 
@@ -18,7 +19,25 @@ def encode_to_base64(datos_binarios):
     print(resultado_b64.decode('utf-8'))
 
 
+
+def decode_from_base64(datos_b64):
+ 
+    # Dato de entrada 
+    print(b{datos_b64})
+
+
+    # Dato b64
+    for byte in datos_b64:
+        print(f"{byte:08b}", end=" ")
+
+    # Decodificación a binario
+    resultado_binary = base64.b64decode(datos_b64)
+
+    # El resultado es un String
+    print(f"\n{resultado_binary}")
+
+
 # Definicion como función main
 if __name__ == "__main__":
-    encode_to_base64(b"Hola Mundo")
-
+   # encode_to_base64(b"Hola Mundo")
+    decode_from_base64("SG9sYSBNdW5kbw==")
